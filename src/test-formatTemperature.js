5f8ca4bb002d1f8cad362640704de073017a1d8e
Evgeni Fridlin <1018598-EvgeniFridlin@users.noreply.replit.com>
2025-12-22 20:10:40 +0000

    feat: normalize wind to km/h and align feels-like, trends, and ship workflow

---
+++ src/test-formatTemperature.js
@@ -0,0 +1,6 @@
+// src/test-formatTemperature.js
+
+const { formatTemperature } = require("./utils/formatTemperature");
+
+console.log(formatTemperature(12, 10));
+console.log(formatTemperature(12, null));
